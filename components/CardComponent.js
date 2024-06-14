531feab13340f58a58c8359fb7e1ebe758413c62
Neasa san <nongsuykady2023@gmail.com>
2024-06-14 22:10:48 +0700

    thrid

--- components/CardComponent.js
+++ components/CardComponent.js
@@ -12,7 +12,7 @@ export const CardComponent = (product)  => {
     </a>
     <div class="px-5 pb-5">
         <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${
+            <h5 class=" line-clamp-1 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${
                 title || "No product title"
             }</h5>
         </a>
@@ -36,8 +36,11 @@ export const CardComponent = (product)  => {
             </div>
         </div>
         <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">${
+            <span class="text-3xl font-bold text-gray-900 dark:text-white">${
+                price ||"Unavaible"
+            }</span>
+            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            ${
                 price ||"Unavaible"
             }</a>
         </div>
